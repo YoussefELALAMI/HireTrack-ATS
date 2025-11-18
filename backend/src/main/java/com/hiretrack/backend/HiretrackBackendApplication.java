a7052942a8264287978f56425afa8fc85030ce0e
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2025-11-18 22:08:00 +0100

    init backend

---
+++ backend/src/main/java/com/hiretrack/backend/HiretrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hiretrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiretrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiretrackBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hiretrack/backend/HiretrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiretrack.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiretrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
