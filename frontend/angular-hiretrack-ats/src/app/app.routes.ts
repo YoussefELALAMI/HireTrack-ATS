c929d9bd1a27267b3f4b33a7bf11887965905c13
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-01-02 17:29:11 +0100

    Apply Guards to Routes

--- frontend/angular-hiretrack-ats/src/app/app.routes.ts
+++ frontend/angular-hiretrack-ats/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './features/auth/register/register.component';
-import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { jwtGuard } from './core/guards/jwt.guard';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
-    {path: '', component: LoginComponent},
-    {path: 'login', component: LoginComponent},
-    {path: 'register', component: RegisterComponent},
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'login', loadComponent: () => import('./features/auth/login/login.component').then(m => m.LoginComponent), canActivate: [guestGuard]},
+    {path: 'register', loadComponent: () => import('./features/auth/register/register.component').then(m => m.RegisterComponent), canActivate: [guestGuard]},
+    {path: 'dashboard', loadComponent: () => import('./features/dashboard/dashboard.component').then(m => m.DashboardComponent), canActivate: [jwtGuard]},
 ];
